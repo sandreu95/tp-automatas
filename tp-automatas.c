dfe7676894abe9f82ae86221132225acd2367dfd
sandreu95 <sandreu@frba.utn.edu.ar>
2023-10-01 18:28:58 -0300

    actualizacion-01-10-23-main

--- tp-automatas.c
+++ tp-automatas.c
@@ -1,8 +1,117 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include ".\automatas.h"
 
+#define DECIMAL 1
+#define OCTAL 2
+#define HEXADECIMAL 3
+#define MAX_TAMANO_PALABRA 100
+
+int operaciones (int operando1, int operando2, char operador) {
+    int resultado;
+    if (operador == '+') {
+        resultado = operando1 + operando2;
+    } else if (operador == '-') {
+        resultado = operando1 - operando2;
+    } else if (operador == '*') {
+        resultado = operando1 * operando2;
+    } else if (operador == '/') {
+        if (operando2 != 0) {
+            resultado = operando1 / operando2;
+        } else {
+            printf("Error: División por cero.\n");
+            return 1;
+        }
+    } else {
+        printf("Operador no reconocido.\n");
+        return 1;
+    }
+    return resultado;
+}
+
+ /*Imprime en pantalla si el automata utilizado reconoció o no la palabra*/
+void printSiReconoce (const char * palabra, int resultado) {
+    if (resultado) {
+        printf("El automata reconocio la palabra %s\n", palabra);
+    } else {
+        printf("El automata NO reconocio la palabra %s\n", palabra);
+    }
+}
+
+/*Elimina el prefijo de la palabra para reconocerlo con el automata correspondiente*/
+char *quitarPrefijo (char *palabra, int longPrefijo) {
+    int i;
+    for (i=0; palabra[i+longPrefijo] != '\0'; i++) {
+        palabra[i] = palabra[i+longPrefijo];
+    }
+    palabra[i] = '\0';
+    return palabra;
+}
+
+/*Verificamos si la palabra que ingresa por variable es un valor decimal, octal o hexadecimal segun su prefijo
+Si el valor empieza con 0 se lo considera octal
+Si el valor comienza con 0x se lo considera hexadecimal
+Si no tiene nunguno de los prefijos anteriores se lo considera decimal
+*/
+int definirTipoDePalabra (char *palabra) {
+    if (palabra[0] != '0' && isdigit(palabra[0])) {
+        return DECIMAL;
+    } else {
+        if (palabra[1] == 'x') {
+            palabra = quitarPrefijo(palabra,2);
+            return HEXADECIMAL;
+        }
+        palabra = quitarPrefijo(palabra,1);
+        return OCTAL;
+    }
+}
+
+/*Dependiendo del tipo de palabra, lo intenta reconocer con el automata correspondiente*/
+int automata (char *palabra) {
+    int tipoDePalabra = definirTipoDePalabra(palabra);
+    if (tipoDePalabra == DECIMAL) {
+        return AutomataDecimal(palabra);
+    } else if (tipoDePalabra == OCTAL) {
+        return AutomataOctal(palabra);
+    }
+    return AutomataHexadecimal(palabra);
+}
+
+int char_to_int (char c) {
+    int valor;
+    if (c <= '9' && c >= '0') {
+        valor = c - '0';
+    }
+    return valor;
+}
 
 int main () {
-    
+    FILE * palabras;
+    palabras = fopen("palabras.txt","r+t");
+    int c;
+    int i = 0;
+    char *palabraNueva = malloc(MAX_TAMANO_PALABRA);
+    /*Recorremos el archivo y creamos una cadena auxiliar que sera la palabra a identificar por el automata
+    Cuando llega al caracter $, como lo lee y lo adhiere a la cadena auxiliar, lo reemplazamos en la cadena por el caracter \0
+    Una vez tenemos la palabra la reconocemos por el automata
+    */    
+    while ((c=fgetc(palabras)) != EOF) {
+        palabraNueva[i] = c;
+        i++;
+        if (c == '$') {
+            palabraNueva[i-1] = '\0';
+            int reconoce = automata(palabraNueva);
+            printSiReconoce (palabraNueva,reconoce);
+            i = 0;
+        }
+    } 
+    palabraNueva[i] = '\0';
+    int reconoce = automata(palabraNueva);
+    printSiReconoce (palabraNueva,reconoce);
+
+    free(palabraNueva);
+    fclose(palabras);
+
     return 0;
 }
